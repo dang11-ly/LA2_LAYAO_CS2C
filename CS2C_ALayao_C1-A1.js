aba4d2efce81eb6f6ae6d7285c5d28bf627a3c75
dang11-ly <layaoamandayvelle@gmail.com>
2024-09-24 10:49:57 +0800

    Add files via upload

---
+++ CS2C_ALayao_C1-A1.js
@@ -0,0 +1,34 @@
+let name = "[Amanda Yvelle Bosque Layao]";
+let birthDate = "[January 11, 2005]";
+let birthPlace = "[Candon General Hospital, Candon City, Ilocos Sur, Philippines 2710]";
+let address = "[Bisangol, Banayoyo, Ilocos Sur, Philippines 2708]";
+let course = "[Bachelor of Science in Computer Science]";
+let year = "2";
+let dreamjob = "[Freelancer Video Editor]";
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + ", and currently living at " + address.toUpperCase()+",");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dream job to be " + dreamjob + " after graduation.");
+
+//Classmate1
+let name1 = "[Renz Galcon Valdez]";
+let birthDate1 = "[April 13, 2005]";
+let birthPlace1 = "[Resonable Hospital, Candon City, Ilocos Sur, Philippines 2710]";
+let address1 = "[Oaig Daya, Candon City, Ilocos Sur, Philippines 2710]";
+let course1 = "[Bachelor of Science in Computer Science]";
+let year1 = "2";
+let dreamjob1 = "[Game Developer]";
+
+console.log(name1.toUpperCase() + " was born " + birthDate1 + " at " + birthPlace1.toUpperCase() + ", and currently living at " + address1.toUpperCase()+",");
+console.log(name1.toLowerCase() + " is taking up " + course1.toLowerCase() + " and dream job to be " + dreamjob1 + " after graduation.");
+
+//Classmate2
+let name2 = "[Jenny Saquiton Naungayan]";
+let birthDate2 = "[June 20, 2024]";
+let birthPlace2 = "[Oiag Daya, Candon City, Ilocos Sur, Philippines 2710]";
+let address2 = "[Oiag Daya, Candon City, Ilocos Sur, Philippines 2710]";
+let course2 = "[Bachelor of Science in Computer Science]";
+let year2 = "2";
+let dreamjob2 = "[Software Engineer]";
+
+console.log(name2.toUpperCase() + " was born " + birthDate2 + " at " + birthPlace2.toUpperCase() + ", and currently living at " + address2.toUpperCase()+",");
+console.log(name2.toLowerCase() + " is taking up " + course2.toLowerCase() + " and dream job to be " + dreamjob2 + " after graduation.");
